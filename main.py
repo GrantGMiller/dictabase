7b44460e5990b2b0b23e7581557cba90c28816b6
Grant Miller <ggm3888@gmail.com>
2019-05-02 11:10:02 -0400

    Add files via upload
    added comments

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import random
 
 
 class Person(_BaseDictabaseTable):
+    # Each subclass of _BaseDictabaseTable produces another table in the db
     pass
 
 
@@ -23,21 +24,27 @@ Drop(Animal)
 
 # Create tables with random data
 for i in range(10):
+    # Instantiating a new Person object adds a new row in the db
     newPerson = Person(
         name='Name{}'.format(i),
         age=random.randint(1, 100),
     )
     print('newPerson=', newPerson)
+
     newAnimal = Animal(
         kind=random.choice(['Cat', 'Dog']),
         name='Fluffy{}'.format(i),
         age=random.randint(1, 10),
     )
+    print('newAnimal=', newAnimal)
 
+# FindAll() returns all items from the database that match
+# you can also use keywords like '_limit', '_reverse', '_orderBy'
 print('Number of animals of age 5: {}'.format(
     len(FindAll(Animal, age=5)))
 )
 
+# FindOne() returns an object found in the database
 person5 = FindOne(Person, name='Name5')
 print('Age of Person5=', person5['age'])
 
@@ -49,4 +56,4 @@ for animal in FindAll(Animal):
 
 print('Remaining Animals=', FindAll(Animal))
 
-# Notice that now you project space contains a MyDatabase.db which is a sqlite database.
+# Notice that now your project space contains a MyDatabase.db which is a sqlite database.
